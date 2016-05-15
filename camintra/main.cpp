9f697c3d8a7a3e27757fe0c12132bfa755cd59f0
uzpei <empiuze@gmail.com>
2016-05-15 18:10:16 -0400

    camintra: move type out

--- camintra/main.cpp
+++ camintra/main.cpp
@@ -13,29 +13,6 @@ using namespace std;
 //hide the local functions in an anon namespace
 namespace {
     
-    string type2str(int type) {
-        string r;
-        
-        uchar depth = type & CV_MAT_DEPTH_MASK;
-        uchar chans = 1 + (type >> CV_CN_SHIFT);
-        
-        switch ( depth ) {
-            case CV_8U:  r = "8U"; break;
-            case CV_8S:  r = "8S"; break;
-            case CV_16U: r = "16U"; break;
-            case CV_16S: r = "16S"; break;
-            case CV_32S: r = "32S"; break;
-            case CV_32F: r = "32F"; break;
-            case CV_64F: r = "64F"; break;
-            default:     r = "User"; break;
-        }
-        
-        r += "C";
-        r += (chans+'0');
-        
-        return r;
-    }
-    
     int state_sharpen = 0;
     int state_denoise_chroma = 0;
     int state_denoise_luma = 0;
